724c575ed1d7e4ba09882b9fb511bd479365f82d
RifeX-LA <timur322132@mail.ru>
2022-04-22 15:53:09 +0300

    Добавил методы для шифрования и расшифрования одного блока. Рефакторинг

--- aes128.h
+++ aes128.h
@@ -10,20 +10,23 @@ enum class mode {ecb, cbc, pcbc, cfb, ofb, ctr};
 
 class aes128 {
     private:
-        using byte_matrix = std::array<std::array<uint8_t, 4>, 4>;
+        using byte_block = std::array<std::array<uint8_t, 4>, 4>;
 
-        std::array<byte_matrix, 11> m_key_schedule;
+        std::array<byte_block, 11> m_key_schedule;
 
-        constexpr void m_sub_bytes(byte_matrix& state) const;
-        constexpr void m_shift_rows(byte_matrix& state) const;
-        constexpr void m_mix_columns(byte_matrix& state) const;
+        constexpr void m_sub_bytes(byte_block& state) const;
+        constexpr void m_shift_rows(byte_block& state) const;
+        constexpr void m_mix_columns(byte_block& state) const;
 
-        constexpr void m_inv_sub_bytes(byte_matrix& state) const;
-        constexpr void m_inv_shift_rows(byte_matrix& state) const;
-        constexpr void m_inv_mix_columns(byte_matrix& state) const;
+        constexpr void m_inv_sub_bytes(byte_block& state) const;
+        constexpr void m_inv_shift_rows(byte_block& state) const;
+        constexpr void m_inv_mix_columns(byte_block& state) const;
 
         constexpr void m_key_expansion(const std::string_view& key);
-        constexpr void m_add_round_key(byte_matrix& state) const;
+        constexpr void m_add_round_key(byte_block& state) const;
+
+        constexpr void m_encrypt_block(byte_block& block) const;
+        constexpr void m_decrypt_block(byte_block& block) const;
 
     public:
         explicit constexpr aes128(const std::string_view& key);
