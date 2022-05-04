df0b2aa965d445a24debf631d99786c59af1f0d4
Maxim Andrievskiy <maxim.andrievskiy2003@gmail.com>
2022-05-04 20:04:36 +0300

    Пофиксил табуляцию #2

--- aes128.cpp
+++ aes128.cpp
@@ -1,142 +1,127 @@
 #include "aes128.h"
 
 void cipher::aes128::m_sub_word(std::array<uint8_t, 4>& word) {
-	std::ranges::transform(word, word.begin(), [](uint8_t byte) {return aes::sbox[byte]; });
+    std::ranges::transform(word, word.begin(), [](uint8_t byte) {return aes::sbox[byte];});
 }
 
 void cipher::aes128::m_rot_word(std::array<uint8_t, 4>& word, std::ptrdiff_t offset) {
-	auto mid_iterator = (offset >= 0) ? word.begin() : word.end();
-	std::ranges::rotate(word, mid_iterator + offset);
-}
-
-uint8_t cipher::aes128::m_gmul(uint8_t a, uint8_t b) {
-	uint8_t p = 0;
-	uint8_t hi_bit_set;
-	for (uint8_t i = 0; i < 8; i++) {
-		if ((b & 1) == 1)
-			p ^= a;
-		hi_bit_set = (a & 0x80);
-		a <<= 1;
-		if (hi_bit_set == 0x80)
-			a ^= 0x1b;
-		b >>= 1;
-	}
-	return p;
+    auto mid_iterator = (offset >= 0) ? word.begin() : word.end();
+    std::ranges::rotate(word, mid_iterator + offset);
 }
 
 void cipher::aes128::m_byte_block_transpose(byte_block& block) {
-	auto temp = block;
-	for (std::size_t i = 0; i < block.size(); ++i) {
-		for (std::size_t j = 0; j < block[i].size(); ++j) {
-			block[i][j] = temp[j][i];
-		}
-	}
+    auto temp = block;
+    for (std::size_t i = 0; i < block.size(); ++i) {
+        for (std::size_t j = 0; j < block[i].size(); ++j) {
+            block[i][j] = temp[j][i];
+        }
+    }
 }
 
 void cipher::aes128::m_key_expansion(const uint8_t* key) {
-	for (std::size_t i = 0; i < m_key_schedule[0].size(); ++i) {
-		for (std::size_t j = 0; j < m_key_schedule[0][i].size(); ++j) {
-			m_key_schedule[0][i][j] = key[m_key_schedule[0].size() * i + j];
-		}
-	}
+    for (std::size_t i = 0; i < m_key_schedule[0].size(); ++i) {
+        for (std::size_t j = 0; j < m_key_schedule[0][i].size(); ++j) {
+            m_key_schedule[0][i][j] = key[m_key_schedule[0].size() * i + j];
+        }
+    }
 
-	for (std::size_t i = 1; i < m_key_schedule.size(); ++i) {
-		for (std::size_t j = 0; j < m_key_schedule[i].size(); ++j) {
-			auto temp = (j == 0) ? m_key_schedule[i - 1].back() : m_key_schedule[i][j - 1];
-			if (j == 0) {
-				m_rot_word(temp, 1);
-				m_sub_word(temp);
-				temp[0] ^= aes::rcon[i - 1];
-			}
-			std::ranges::transform(m_key_schedule[i - 1][j], temp, m_key_schedule[i][j].begin(), std::bit_xor<>());
-		}
-	}
+    for (std::size_t i = 1; i < m_key_schedule.size(); ++i) {
+        for (std::size_t j = 0; j < m_key_schedule[i].size(); ++j) {
+            auto temp = (j == 0) ? m_key_schedule[i - 1].back() : m_key_schedule[i][j - 1];
+            if (j == 0) {
+                m_rot_word(temp, 1);
+                m_sub_word(temp);
+                temp[0] ^= aes::rcon[i - 1];
+            }
+            std::ranges::transform(m_key_schedule[i - 1][j], temp, m_key_schedule[i][j].begin(), std::bit_xor<>());
+        }
+    }
 
-	std::ranges::for_each(m_key_schedule, m_byte_block_transpose);
+    std::ranges::for_each(m_key_schedule, m_byte_block_transpose);
 }
 
 void cipher::aes128::m_sub_bytes(byte_block& state) {
-	std::ranges::for_each(state, m_sub_word);
+    std::ranges::for_each(state, m_sub_word);
 }
 
 void cipher::aes128::m_shift_rows(byte_block& state) {
-	for (std::ptrdiff_t i = 1; i < state.size(); ++i) {
-		m_rot_word(state[i], i);
-	}
+    for (std::ptrdiff_t i = 1; i < state.size(); ++i) {
+        m_rot_word(state[i], i);
+    }
 }
 
 void cipher::aes128::m_multiply_matrix_by_columns(byte_block& state, const byte_block& op_table) {
-	for (std::size_t i = 0; i < state.size(); ++i) {
-		uint8_t s0 = 0, s1 = 0, s2 = 0, s3 = 0;
-		for (std::size_t j = 0; j < state.size(); ++j) {
-			s0 ^= m_gmul(state[j][i], op_table[0][j]);
-			s1 ^= m_gmul(state[j][i], op_table[1][j]);
-			s2 ^= m_gmul(state[j][i], op_table[2][j]);
-			s3 ^= m_gmul(state[j][i], op_table[3][j]);
-		}
-		state[0][i] = s0;
-		state[1][i] = s1;
-		state[2][i] = s2;
-		state[3][i] = s3;
-	}
+    for (std::size_t i = 0; i < state.size(); ++i) {
+        uint8_t s0 = 0, s1 = 0, s2 = 0, s3 = 0;
+        for (std::size_t j = 0; j < state.size(); ++j) {
+            s0 ^= m_gmul(state[j][i], op_table[0][j]);
+            s1 ^= m_gmul(state[j][i], op_table[1][j]);
+            s2 ^= m_gmul(state[j][i], op_table[2][j]);
+            s3 ^= m_gmul(state[j][i], op_table[3][j]);
+        }
+        state[0][i] = s0;
+        state[1][i] = s1;
+        state[2][i] = s2;
+        state[3][i] = s3;
+    }
 }
 
 void cipher::aes128::m_mix_columns(byte_block& state) {
-	m_multiply_matrix_by_columns(state, aes::mix_columns_op);
+    m_multiply_matrix_by_columns(state, aes::mix_columns_op);
 }
 
 void cipher::aes128::m_inv_sub_bytes(byte_block& state) {
-	for (auto& word : state) {
-		std::ranges::transform(word, word.begin(), [](uint8_t byte) {return aes::inv_sbox[byte]; });
-	}
+    for (auto& word : state) {
+        std::ranges::transform(word, word.begin(), [](uint8_t byte) {return aes::inv_sbox[byte];});
+    }
 }
 
 void cipher::aes128::m_inv_shift_rows(byte_block& state) {
-	for (std::ptrdiff_t i = 1; i < state.size(); ++i) {
-		m_rot_word(state[i], -i);
-	}
+    for (std::ptrdiff_t i = 1; i < state.size(); ++i) {
+        m_rot_word(state[i], -i);
+    }
 }
 
 void cipher::aes128::m_inv_mix_columns(byte_block& state) {
-	m_multiply_matrix_by_columns(state, aes::inv_mix_columns_op);
+    m_multiply_matrix_by_columns(state, aes::inv_mix_columns_op);
 }
 
 void cipher::aes128::m_xor_blocks(byte_block& lhs, const byte_block& rhs) {
-	for (std::size_t i = 0; i < lhs.size(); ++i) {
-		std::ranges::transform(lhs[i], rhs[i], lhs[i].begin(), std::bit_xor<>());
-	}
+    for (std::size_t i = 0; i < lhs.size(); ++i) {
+        std::ranges::transform(lhs[i], rhs[i], lhs[i].begin(), std::bit_xor<>());
+    }
 }
 
 void cipher::aes128::m_encrypt_block(byte_block& block) const {
-	m_xor_blocks(block, m_key_schedule[0]);
+    m_xor_blocks(block, m_key_schedule[0]);
 
-	for (std::size_t i = 1; i < m_rounds; ++i) {
-		m_sub_bytes(block);
-		m_shift_rows(block);
-		m_mix_columns(block);
-		m_xor_blocks(block, m_key_schedule[i]);
-	}
+    for (std::size_t i = 1; i < m_rounds; ++i) {
+        m_sub_bytes(block);
+        m_shift_rows(block);
+        m_mix_columns(block);
+        m_xor_blocks(block, m_key_schedule[i]);
+    }
 
-	m_sub_bytes(block);
-	m_shift_rows(block);
-	m_xor_blocks(block, m_key_schedule.back());
+    m_sub_bytes(block);
+    m_shift_rows(block);
+    m_xor_blocks(block, m_key_schedule.back());
 }
 
 void cipher::aes128::m_decrypt_block(byte_block& block) const {
-	m_xor_blocks(block, m_key_schedule.back());
+    m_xor_blocks(block, m_key_schedule.back());
 
-	for (std::size_t i = m_rounds - 1; i > 0; --i) {
-		m_inv_sub_bytes(block);
-		m_inv_shift_rows(block);
-		m_xor_blocks(block, m_key_schedule[i]);
-		m_inv_mix_columns(block);
-	}
+    for (std::size_t i = m_rounds - 1; i > 0; --i) {
+        m_inv_sub_bytes(block);
+        m_inv_shift_rows(block);
+        m_xor_blocks(block, m_key_schedule[i]);
+        m_inv_mix_columns(block);
+    }
 
-	m_inv_sub_bytes(block);
-	m_inv_shift_rows(block);
-	m_xor_blocks(block, m_key_schedule[0]);
+    m_inv_sub_bytes(block);
+    m_inv_shift_rows(block);
+    m_xor_blocks(block, m_key_schedule[0]);
 }
 
 cipher::aes128::aes128(const std::string_view& key) {
-	m_key_expansion(MD5(key).decimal_digest());
+    m_key_expansion(MD5(key).decimal_digest());
 }
