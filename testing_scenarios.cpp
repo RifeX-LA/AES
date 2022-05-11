1ad6535c02699e5b15b81767c1beaa14c6c6466e
darashi <darashi003@gmail.com>
2022-05-11 11:41:03 +0300

    Добавила функцию генерации строки с маленьким или большим весом Хэмминга

--- testing_scenarios.cpp
+++ testing_scenarios.cpp
@@ -16,3 +16,24 @@ std::string random_string(std::size_t size) {
 
     return str;
 }
+
+std::string generate_heavy_or_little_weight_string_block(int start_elem) {
+	std::mt19937 gen(std::random_device{}());
+	std::uniform_int_distribution<int> random_symbol_from_minus128_to_127(-128, 127);
+
+	std::string string_block(16, (256 - start_elem) % 256);
+
+	std::uniform_int_distribution<int> random_symbol_from_0_to_2(0, 2);
+	int weight = random_symbol_from_0_to_2(gen);
+
+	std::uniform_int_distribution<int> random_symbol_from_0_to_3(0, 3);
+	std::uniform_int_distribution<int> random_bit_from_0_to_7(0, 7);
+	for (int k = 0; k < weight; k++) {
+		int i = random_symbol_from_0_to_3(gen);
+		int j = random_symbol_from_0_to_3(gen);
+		int position_of_bit_to_change = random_bit_from_0_to_7(gen);
+		char pow_of_two = pow(2, position_of_bit_to_change);
+		string_block[i * 4 + j] ^= pow_of_two;
+	}
+	return string_block;
+}
--- testing_scenarios.h
+++ testing_scenarios.h
@@ -4,3 +4,5 @@
 
 std::string xor_strings(const std::string_view& lhs, const std::string_view& rhs);
 std::string random_string(std::size_t size);
+
+std::string generate_heavy_or_little_weight_string_block(int start_elem);
