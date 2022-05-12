9de5026abd9e10cbe32a4b311481e865591914da
Maxim Andrievskiy <maxim.andrievskiy2003@gmail.com>
2022-05-12 13:54:15 +0300

    Добавил сценарии тестирования для корреляции открытого и шифр текста, цепочной обработки блоков

--- testing_scenarios.cpp
+++ testing_scenarios.cpp
@@ -18,24 +18,24 @@ std::string random_string(std::size_t size) {
 }
 
 std::string generate_heavy_or_little_weight_string_block(int start_elem) {
-	std::mt19937 gen(std::random_device{}());
-	std::uniform_int_distribution<int> random_symbol_from_minus128_to_127(-128, 127);
+    std::mt19937 gen(std::random_device{}());
+    std::uniform_int_distribution<int> random_symbol_from_minus128_to_127(-128, 127);
 
-	std::string string_block(16, (256 - start_elem) % 256);
+    std::string string_block(16, (256 - start_elem) % 256);
 
-	std::uniform_int_distribution<int> random_symbol_from_0_to_2(0, 2);
-	int weight = random_symbol_from_0_to_2(gen);
+    std::uniform_int_distribution<int> random_symbol_from_0_to_2(0, 2);
+    int weight = random_symbol_from_0_to_2(gen);
 
-	std::uniform_int_distribution<int> random_symbol_from_0_to_3(0, 3);
-	std::uniform_int_distribution<int> random_bit_from_0_to_7(0, 7);
-	for (int k = 0; k < weight; k++) {
-		int i = random_symbol_from_0_to_3(gen);
-		int j = random_symbol_from_0_to_3(gen);
-		int position_of_bit_to_change = random_bit_from_0_to_7(gen);
-		char pow_of_two = pow(2, position_of_bit_to_change);
-		string_block[i * 4 + j] ^= pow_of_two;
-	}
-	return string_block;
+    std::uniform_int_distribution<int> random_symbol_from_0_to_3(0, 3);
+    std::uniform_int_distribution<int> random_bit_from_0_to_7(0, 7);
+    for (int k = 0; k < weight; k++) {
+        int i = random_symbol_from_0_to_3(gen);
+        int j = random_symbol_from_0_to_3(gen);
+        int position_of_bit_to_change = random_bit_from_0_to_7(gen);
+        char pow_of_two = pow(2, position_of_bit_to_change);
+        string_block[i * 4 + j] ^= pow_of_two;
+    }
+    return string_block;
 }
 
 std::string low_or_heavy_weight_plaintext_random_key(int start_elem) {
@@ -125,5 +125,25 @@ std::string plain_text_errors_random_key() {
         result_str += xor_strings(encrypted, encrypted_error_plain_text);
     }
 
+    return result_str;
+}
+
+std::string plain_text_and_chipertext_correlation() {
+    std::string plain_text = random_string(bytes_in_megabyte);
+    std::string ciphertext = cipher::aes128(random_string(16)).encrypt(plain_text).substr(0, bytes_in_megabyte);
+    return xor_strings(plain_text, ciphertext);
+}
+
+std::string block_chain_processing() {
+    std::string result_str;
+    result_str.reserve(bytes_in_megabyte);
+    std::string plain_text(16, 0);
+    std::string key = random_string(16);
+    std::string ciphertext = cipher::aes128(key).encrypt(plain_text).substr(0, 16);
+    result_str += ciphertext;
+    for (std::size_t i = 0; i < bytes_in_megabyte / 16; ++i) {
+        ciphertext = cipher::aes128(key).encrypt(ciphertext).substr(0, 16);
+        result_str += ciphertext;
+    }
     return result_str;
 }
--- testing_scenarios.h
+++ testing_scenarios.h
@@ -8,10 +8,8 @@ std::string xor_strings(const std::string_view& lhs, const std::string_view& rhs
 std::string random_string(std::size_t size);
 
 std::string generate_heavy_or_little_weight_string_block(int start_elem);
-
 std::string low_or_heavy_weight_plaintext_random_key(int start_elem);
 std::string random_plaintext_low_or_heavy_weight_key(int start_elem);
-
 std::string low_weight_plaintext_random_key();
 std::string heavy_weight_plaintext_random_key();
 std::string random_plaintext_low_weight_key();
@@ -20,3 +18,6 @@ std::string random_plaintext_heavy_weight_key();
 std::string random_plain_text_and_key();
 std::string random_plain_text_key_errors();
 std::string plain_text_errors_random_key();
+
+std::string plain_text_and_chipertext_correlation();
+std::string block_chain_processing();
