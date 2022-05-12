9e87efa5f893d88145e8bb27aa8aaa753fd27b70
RifeX-LA <timur322132@mail.ru>
2022-05-12 12:20:24 +0300

    Добавил сценарий с рандомными текстом и ключом, с ошибкой в тексте и в ключе

--- testing_scenarios.cpp
+++ testing_scenarios.cpp
@@ -42,12 +42,11 @@ std::string low_or_heavy_weight_plaintext_random_key(int start_elem) {
     int blocks_of_plain_text_num = 65536;
     std::string key = random_string(16);
     std::string ciphertext;
-    int number_bytes_in_megabyte = 1048576;
-    ciphertext.reserve(number_bytes_in_megabyte);
+    ciphertext.reserve(bytes_in_megabyte);
 
     for (int i = 0; i < blocks_of_plain_text_num; i++) {
         std::string string_block = generate_heavy_or_little_weight_string_block(start_elem);
-        cipher::aes128 aes = cipher::aes128::aes128(key);
+        cipher::aes128 aes(key);
         ciphertext += aes.encrypt(string_block, cipher::mode::ecb, false);
     }
     return ciphertext;
@@ -57,12 +56,11 @@ std::string random_plaintext_low_or_heavy_weight_key(int start_elem) {
     int blocks_of_plain_text_num = 65536;
     std::string key = generate_heavy_or_little_weight_string_block(start_elem);
     std::string ciphertext;
-    int number_bytes_in_megabyte = 1048576;
-    ciphertext.reserve(number_bytes_in_megabyte);
+    ciphertext.reserve(bytes_in_megabyte);
 
     for (int i = 0; i < blocks_of_plain_text_num; i++) {
         std::string string_block = random_string(16);
-        cipher::aes128 aes = cipher::aes128::aes128(key);
+        cipher::aes128 aes(key);
         ciphertext += aes.encrypt(string_block, cipher::mode::ecb, false);
     }
     return ciphertext;
@@ -82,4 +80,50 @@ std::string random_plaintext_low_weight_key() {
 
 std::string random_plaintext_heavy_weight_key() {
     return random_plaintext_low_or_heavy_weight_key(1);
+}
+
+std::string random_plain_text_and_key() {
+    cipher::aes128 aes(random_string(16));
+    return aes.encrypt(random_string(bytes_in_megabyte)).substr(0, bytes_in_megabyte);
+}
+
+std::string random_plain_text_key_errors() {
+    constexpr std::size_t key_len = 16;
+    constexpr std::size_t plain_text_size = bytes_in_megabyte / key_len;
+
+    std::string text(plain_text_size, 0);
+    std::string result_str;
+    result_str.reserve(bytes_in_megabyte);
+
+    for (std::size_t i = 0; i < key_len; ++i) {
+        std::string key = random_string(key_len);
+
+        std::string encrypted = cipher::aes128(key).encrypt(text).substr(0, plain_text_size);
+        key[i] ^= 1;
+        std::string encrypted_error_key = cipher::aes128(key).encrypt(text).substr(0, plain_text_size);
+
+        result_str += xor_strings(encrypted, encrypted_error_key);
+    }
+
+    return result_str;
+}
+
+std::string plain_text_errors_random_key() {
+    const std::size_t plain_text_size = std::sqrt(bytes_in_megabyte);
+
+    cipher::aes128 aes(std::string(16, 0));
+    std::string result_str;
+    result_str.reserve(bytes_in_megabyte);
+
+    for (std::size_t i = 0; i < plain_text_size; ++i) {
+        std::string plain_text = random_string(plain_text_size);
+
+        std::string encrypted = aes.encrypt(plain_text).substr(0, plain_text_size);
+        plain_text[i] ^= 1;
+        std::string encrypted_error_plain_text = aes.encrypt(plain_text).substr(0, plain_text_size);
+
+        result_str += xor_strings(encrypted, encrypted_error_plain_text);
+    }
+
+    return result_str;
 }
